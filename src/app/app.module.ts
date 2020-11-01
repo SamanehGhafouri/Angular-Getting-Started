ee9eb5e132f26359ddc07d15271c29a5ee9a6cb5
SamanehGhafouri <samaneh2010gh@gmail.com>
2020-11-01 15:22:20 -0500

    store application is competed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,7 @@ import { ProductDetailsComponent } from './product-details/product-details.compo
 import { CartComponent } from './cart/cart.component';
 import {HttpClientModule} from "@angular/common/http";
 import { ShippingComponent } from './shipping/shipping.component';
+import {ReactiveFormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
@@ -26,7 +27,8 @@ import { ShippingComponent } from './shipping/shipping.component';
     BrowserModule,
     HttpClientModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
